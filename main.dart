913b16a2fce3d26cef7481b552e3d3400073f9b0
Che-t <wekesacharity@gmail.com>
2024-04-04 17:51:21 +0300

    Add main.dart demonstrating Dart data types

---
+++ main.dart
@@ -0,0 +1,27 @@
+// Dart program demonstrating data types
+
+void main() {
+  // Integer data type
+  int age = 28; // Represents age as an integer
+  print('Age: $age');
+
+  // Double data type
+  double weight = 70.3; // Represents weight as a double
+  print('Weight: $weight');
+
+  // String data type
+  String name = 'Chet'; // Represents name as a string
+  print('Name: $name');
+
+  // List data type
+  List<int> numbers = [1, 2, 3, 4, 5]; // Represents a list of integers
+  print('Numbers: $numbers');
+
+  // Map data type
+  Map<String, dynamic> person = {
+    'name': 'Mercy',
+    'age': 29,
+    'isStudent': true,
+  }; // Represents information about a person
+  print('Person: $person');
+}
